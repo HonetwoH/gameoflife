a6229e6117c9330878a3478f8f415032fca7f979
H12H <sucodetr@protonmail.com>
2024-03-18 15:57:17 +0530

    completed the game

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use crossterm::{
     cursor, execute,
     style::{self, Stylize},
-    terminal::{size, EnterAlternateScreen, LeaveAlternateScreen, SetSize},
+    terminal::{size, Clear, ClearType, EnterAlternateScreen, LeaveAlternateScreen, SetSize},
     QueueableCommand,
 };
 use nanorand::{Rng, WyRand};
 use std::env;
-use std::io::{stdout, Write};
+use std::io::{stdout, Stdout, Write};
 use std::ops::Div;
 use std::{thread, time};
 
@@ -21,7 +21,7 @@ fn main() {
             .expect("parsing of cells failed");
         let tdelay = argument[1].parse::<u64>().expect("parsing of dealy failed");
         let maxgens = argument[2].parse::<usize>().expect("parsing gen failed");
-        let delay = time::Duration::from_secs(1 / 60);
+        let delay = time::Duration::from_millis(tdelay);
         let (xmax, ymax) = {
             let (xmax, ymax) = size().expect("Failed at getting size");
             (xmax as usize, ymax as usize)
@@ -34,15 +34,18 @@ fn main() {
 
         // set terminal
         execute!(stdout(), EnterAlternateScreen).expect("Cannot spawn new window");
+        execute!(stdout(), Clear(ClearType::All)).expect("Failed to clear screen");
         execute!(stdout(), cursor::Hide).expect("Failed to hide cursor");
         execute!(stdout(), SetSize(xmax as u16, ymax as u16)).expect("Cannot resize window");
+
+        setup_borders(&mut stdout(), max);
 
         // loop
         let mut gen: usize = 1;
         while gen <= maxgens {
             next_generation(&mut cells, gen, max);
             thread::sleep(delay);
-            display(&cells, gen, max);
+            display(&cells, &mut stdout(), gen, max);
             gen += 1;
         }
 
@@ -53,27 +56,87 @@ fn main() {
     }
 }
 
-fn display(cells: &Vec<Vec<[bool; 2]>>, gen: usize, (xmax, ymax): (usize, usize)) {
-    let (current, previous) = (gen % 2, (gen + 1) % 2);
-    let mut stdout = stdout();
+fn setup_borders(stdout: &mut Stdout, (xmax, ymax): (usize, usize)) {
+    // some more setup ///////////////////////////////////
     stdout
-        .queue(cursor::MoveTo(3, 0))
+        .queue(cursor::MoveTo(0, 0))
+        .expect("panicked while setting cursor window")
+        .queue(style::PrintStyledContent("┌".grey()))
+        .expect("panciked while printing window")
+        .queue(cursor::MoveTo(xmax as u16, 0))
+        .expect("panicked while setting cursor window")
+        .queue(style::PrintStyledContent("┐".grey()))
+        .expect("panciked while printing window")
+        .queue(cursor::MoveTo(0, ymax as u16))
+        .expect("panicked while setting cursor window")
+        .queue(style::PrintStyledContent("└".grey()))
+        .expect("panciked while printing window")
+        .queue(cursor::MoveTo(xmax as u16, ymax as u16))
+        .expect("panicked while setting cursor window")
+        .queue(style::PrintStyledContent("┘".grey()))
+        .expect("panciked while printing window");
+
+    for x in 1..xmax - 1 {
+        stdout
+            .queue(cursor::MoveTo(x as u16, 0))
+            .expect("panicked while setting cursor window")
+            .queue(style::PrintStyledContent("─".grey()))
+            .expect("panciked while printing window")
+            .queue(cursor::MoveTo(x as u16, ymax as u16))
+            .expect("panicked while setting cursor window")
+            .queue(style::PrintStyledContent("─".grey()))
+            .expect("panciked while printing window");
+    }
+
+    for y in 1..ymax - 1 {
+        stdout
+            .queue(cursor::MoveTo(0, y as u16))
+            .expect("panicked while setting cursor window")
+            .queue(style::PrintStyledContent("│".grey()))
+            .expect("panciked while printing window")
+            .queue(cursor::MoveTo(xmax as u16, y as u16))
+            .expect("panicked while setting cursor window")
+            .queue(style::PrintStyledContent("│".grey()))
+            .expect("panciked while printing window");
+    }
+    let title = " GAME OF LIFE ";
+    let l = title.len() / 2;
+    let offset = xmax / 2 - l;
+
+    stdout
+        .queue(cursor::MoveTo(offset as u16, 0))
+        .expect("Cannot move cursor")
+        .queue(style::PrintStyledContent(title.grey()))
+        .expect("Failed to add title.");
+}
+
+fn display(
+    cells: &Vec<Vec<[bool; 2]>>,
+    stdout: &mut Stdout,
+    gen: usize,
+    (xmax, ymax): (usize, usize),
+) {
+    let (current, previous) = (gen % 2, (gen + 1) % 2);
+    stdout
+        .queue(cursor::MoveTo(3, ymax as u16))
         .expect("paniced which setting gen")
-        .queue(style::Print(format!("{:03}", gen)))
+        .queue(style::Print(format!(" Gen: {:04} ", gen)))
         .expect("paniced which setting gen");
 
     for y in 1..ymax - 1 {
         for x in 1..xmax - 1 {
-            let ch = if cells[y][x][current] {
-                "O".magenta()
-            } else {
-                " ".white()
-            };
-            stdout
-                .queue(cursor::MoveTo(x as u16, y as u16))
-                .expect("cursor jammed !")
-                .queue(style::PrintStyledContent(ch))
-                .expect("problem in drawing");
+            if cells[y][x][current] != cells[y][x][previous] {
+                let ch = if cells[y][x][current] {
+                    "+".magenta()
+                } else {
+                    " ".white()
+                };
+                stdout
+                    .queue(cursor::MoveTo(x as u16, y as u16))
+                    .expect("cursor jammed !")
+                    .queue(style::PrintStyledContent(ch))
+                    .expect("problem in drawing");
+            }
         }
     }
     stdout.flush().expect("flushing failed");
@@ -98,18 +161,18 @@ fn next_generation(cells: &mut Vec<Vec<[bool; 2]>>, gen: usize, (xmax, ymax): (u
         for x in 0..xmax {
             let alive_neighbors = alive_neighbors(&cells, gen, (y, x), (xmax, ymax));
             cells[y][x][current] = if cells[y][x][previous] {
-                // TODO:
+                // for alive
                 match alive_neighbors {
-                    0..=1 => (true, false), // dead
-                    2..=3 => (true, true),  // alive
-                    4..=8 => (true, false), // overpopulation
+                    0 | 1 => false, // dead
+                    2 | 3 => true,  // alive
+                    4..=8 => false, // overpopulation
                     _ => panic!("phantom neighbors"),
                 }
             } else {
+                // for dead
                 match alive_neighbors {
-                    3 => (true, true),
-                    0..=2 => (true, false),
-                    4..=8 => (true, false),
+                    3 => true,
+                    x if x <= 8 => false,
                     _ => panic!("phantom neighbors"),
                 }
             }
